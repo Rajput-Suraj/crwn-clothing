17cacf78558f2d6ef1367cf20c6600a0cfa5ea69
Rajput-Suraj <surajarajput@gmail.com>
2020-11-13 09:52:24 +0530

    Added directory and menu item components with respective styling of that components.

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import Directory from "../../components/directory/Directory";
+import "./Homepage.scss";
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <Directory></Directory>
+        </div>
+    );
+};
+
+export default Homepage;
